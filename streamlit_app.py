fbbfdb74a155b0ace0e5cb92aeaf96c44b4852ac
gi-cpu <58240702+gi-cpu@users.noreply.github.com>
2026-08-25 13:17:07 +1000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,17 +34,8 @@ if time_to_insert:
     session.sql(my_insert_stmt).collect()
     st.success('Your Smoothie is ordered!', icon="✅")
 
-
-
-
-
-
-
-
-
-
-
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response.json())
+#st.text(smoothiefroot_response.json())
+sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
 
